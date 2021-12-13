91f60e7af2527b0f1701f2574632846336778469
Danperad <kokorin0521@gmail.com>
2021-12-13 22:38:12 +0300

    add new pages

---
+++ PhotostudioGUI/Pages/ServicePage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using PhotostudioDLL.Entity;
+
+
+namespace PhotostudioGUI.Pages;
+
+/// <summary>
+/// Логика взаимодействия для ServicePage.xaml
+/// </summary>
+public partial class ServicePage : Page
+{
+    public ServicePage()
+    {
+        InitializeComponent();
+        ServiceData.ItemsSource = Service.Get();
+    }
+
+    private void AddServiceClick(object sender, RoutedEventArgs e)
+    {
+        Service service = new Service
+        {
+            Title = TitleBox.Text,
+            Description = DescriptionBox.Text,
+            Price = Convert.ToDecimal(PriceBox.Text)
+        };
+        Service.Add(service);
+        ServiceData.ItemsSource = Service.Get();
+    }
+}
